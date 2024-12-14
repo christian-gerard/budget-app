4e2fd07deb90014d1702965c1f13aba020c5295c
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-12-13 20:04:50 -0700

    Updating Phoenix Project

--- phx_app/mix.exs
+++ phx_app/mix.exs
@@ -57,7 +57,9 @@ defmodule PhxApp.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:hammox, "~> 0.7", only: :test}
+
     ]
   end
 
